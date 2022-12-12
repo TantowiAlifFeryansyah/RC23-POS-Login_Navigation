678379b1aa7bf15992aaa6b1cf429513c21991f4
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2022-12-12 23:34:33 +0700

    c23 pos

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+module.exports =  function(db) {
+router.get('/', function(req, res, next) {
+  res.render('login');
+});
+
+return router;
+}
