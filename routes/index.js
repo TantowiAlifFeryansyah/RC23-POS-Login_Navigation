d2a80460d28b660d6c17e8f44e1d13eacd90bd84
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2022-12-13 23:39:45 +0700

    c23 partials

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,16 @@ router.get('/', function(req, res, next) {
   res.render('login');
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.post('/register', function(req, res, next) {
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index');
+});
+
 return router;
 }
