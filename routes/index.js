4096a0c88fb097daa0a5f9409d3401d2f764f761
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2022-12-14 16:32:03 +0700

    c23 partials

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 /* GET home page. */
 module.exports =  function(db) {
