24b5cfc7555ab7826e6e53641b8ecdf774a2fdf0
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2022-12-15 01:15:12 +0700

    session flash bcryp

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
+var flash = require('connect-flash');
 
 // connect postgres
 const { Pool } = require('pg');
@@ -28,6 +30,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'tantowi',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,69 @@ var express = require('express');
 var router = express.Router();
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const helpers = require('../helpers/util')
 
 /* GET home page. */
-module.exports =  function(db) {
-router.get('/', function(req, res, next) {
-  res.render('login');
-});
+module.exports = function (db) {
+  router.get('/', function (req, res, next) {
+    res.render('login', {info: req.flash('info')});
+  });
 
-router.get('/register', function(req, res, next) {
-  res.render('register');
-});
+  router.post('/', function (req, res) {
+    const { email, password } = req.body
+    db.query('SELECT * FROM users where email = $1', [email], (err, data) => {
+      if (err) return res.send('email ', err)
 
-router.post('/register', function(req, res, next) {
-});
+      if (data.rows.length == 0) {
+        req.flash('info', 'Email tidak terdaftar')
+        return res.redirect('/')
+      }
 
-router.get('/home', function(req, res, next) {
-  res.render('index');
-});
+      bcrypt.compare(password, data.rows[0].password, function(err, result) {
+      if (err) return res.send(err)
 
-return router;
+      if (!result) {
+        req.flash('info', 'Password salah')
+        return res.redirect('/')
+      }
+
+      req.session.user = data.rows[0]
+      res.redirect('/home')
+      });
+
+    });
+  });
+
+  router.get('/register', function (req, res, next) {
+    res.render('register');
+  });
+
+  router.post('/register', function (req, res, next) {
+    const { email, name, password, role } = req.body
+    db.query('SELECT * FROM users where email = $1', [email], (err, data) => {
+      if (err) return res.send('email ', err)
+
+      if (data.rows.length > 0) return res.send('email sudah terdaftar')
+
+      bcrypt.hash(password, saltRounds, function (err, hash) {
+        if (err) return res.send(err)
+        db.query('INSERT INTO users (email, name, password, role) VALUES ($1, $2, $3, $4)', [email, name, hash, role], (err, data) => {
+          if (err) return res.send('password ', err)
+          res.redirect('/')
+        })
+      });
+    })
+  });
+
+  router.get('/logout', helpers.isLoggedIn,function (req, res, next) {
+    req.session.destroy(function(err){
+      res.redirect('/');
+    })
+  });
+
+  router.get('/home', helpers.isLoggedIn,function (req, res, next) {
+    res.render('index');
+  });
+
+  return router;
 }
